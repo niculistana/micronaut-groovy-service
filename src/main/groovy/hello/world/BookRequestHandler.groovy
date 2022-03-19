8b7b2a0a5edff149a94725f3fe67d38796853524
Nicu Listana <niculistana@gmail.com>
2022-03-19 00:39:51 -0700

    run micronaut groovy generator

---
+++ src/main/groovy/hello/world/BookRequestHandler.groovy
@@ -0,0 +1,17 @@
+package hello.world
+import io.micronaut.core.annotation.Introspected
+import io.micronaut.function.aws.MicronautRequestHandler
+
+@Introspected
+class BookRequestHandler extends MicronautRequestHandler<Book, BookSaved> {
+
+    @Override
+    BookSaved execute(Book input) {
+        BookSaved bookSaved = new BookSaved()
+        bookSaved.with {
+            name = input.getName()
+            isbn = UUID.randomUUID().toString()
+        }
+        bookSaved
+    }
+}
